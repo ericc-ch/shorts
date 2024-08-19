ed02add1a98d508a45627b0a646d34c0a80fcddc
Erick Christian <erickchristian48@gmail.com>
2024-08-19 08:45:28 +0700

    config: setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["eslint.config.*"],
+  },
+
+  eslint.configs.recommended,
+  ...tseslint.configs.strict
+);
