ac9c7f749bb7e871e721e41113584c87fc5308aa
Erick Christian <erickchristian48@gmail.com>
2024-08-19 23:48:40 +0700

    feat(script-gen): delete uploaded files on startup

--- apps/script-gen/src/index.ts
+++ apps/script-gen/src/index.ts
@@ -3,6 +3,9 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { SERVER_PORT } from "./lib/env";
 import { routes } from "./routes";
+import { deleteUploadedFiles } from "./lib/files";
+
+await deleteUploadedFiles();
 
 const app = new Hono();
 app.use("*", logger());
