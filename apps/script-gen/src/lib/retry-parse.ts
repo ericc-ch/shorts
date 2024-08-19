4d2a398c250a30d3a59b2fcc15a1674734381f2e
Erick Christian <erickchristian48@gmail.com>
2024-08-19 23:48:23 +0700

    feat(script-gen): retry parsing for gemini response

---
+++ apps/script-gen/src/lib/retry-parse.ts
@@ -0,0 +1,25 @@
+import consola from "consola";
+import { safeDestr } from "destr";
+
+interface Options {
+  fn: () => string | Promise<string>;
+  retries: number;
+}
+
+export const retryParse = async ({ fn, retries }: Options) => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      consola.log(`Parsing JSON: ${i + 1}/${retries}`);
+      const jsonString = await fn();
+      const result = safeDestr(jsonString);
+
+      return result;
+    } catch (e) {
+      if (i < retries) {
+        consola.warn(`Failed to parse JSON: ${e}`);
+      } else {
+        throw e;
+      }
+    }
+  }
+};
