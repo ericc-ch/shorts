f7cbfff9e21cced9688f7bb9c53208e6debf6373
Erick Christian <erickchristian48@gmail.com>
2024-08-19 08:17:11 +0700

    test(common): write tests

---
+++ packages/common/tests/env.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect, afterAll, beforeAll } from "bun:test";
+import { env } from "../src";
+
+describe("env()", () => {
+  it("should return the value of an environment variable", () => {
+    const key = "TEST_VAR";
+    const value = "test-value";
+
+    import.meta.env[key] = value;
+
+    expect(env(key)).toBe(value);
+
+    delete import.meta.env[key];
+  });
+
+  it("should throw an error if the environment variable is not set", () => {
+    const key = "NON_EXISTENT_VAR";
+
+    expect(() => env(key)).toThrowError(
+      `Environment variable ${key} is not set`
+    );
+  });
+});
---
+++ packages/common/tests/temp-file.test.ts
@@ -0,0 +1,53 @@
+import { describe, expect, it } from "bun:test";
+import { TempFileManager } from "../src";
+
+describe("TempFileManager", () => {
+  const dir = "test-temp-files";
+  const manager = new TempFileManager({ dir });
+
+  it("should create a temp file and write data to it", async () => {
+    const filename = "test_file";
+    const data = "Hello, world!";
+
+    const filePath = await manager.create(filename, data);
+
+    const fileData = await Bun.file(filePath).text();
+    expect(fileData).toBe(data);
+
+    await manager.delete(filename);
+  });
+
+  it("should delete the created temp file", async () => {
+    const filename = "test_file";
+    const data = "Hello, world!";
+
+    const filePath = await manager.create(filename, data);
+
+    await manager.delete(filename);
+
+    const fileExists = await Bun.file(filePath).exists();
+
+    expect(fileExists).toBe(false);
+  });
+
+  it("should delete all temp files", async () => {
+    const fileNames = Array(10)
+      .fill(undefined)
+      .map((_, i) => `test_file_${i}`);
+    const data = "Hello, world!";
+
+    const createPromises = fileNames.map((filename) =>
+      manager.create(filename, data)
+    );
+
+    const results = await Promise.all(createPromises);
+
+    await manager.deleteAll();
+
+    const filesExistences = await Promise.all(
+      results.map((filePath) => Bun.file(filePath).exists())
+    );
+
+    expect(filesExistences.every((exists) => !exists)).toBe(true);
+  });
+});
