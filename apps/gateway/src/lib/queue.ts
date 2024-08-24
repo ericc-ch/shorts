641b3f60f28abc728a72babd324d928967f6ea51
Erick Christian <erickchristian48@gmail.com>
2024-08-25 00:06:19 +0700

    feat(gateway): use env for amqp conn

--- apps/gateway/src/lib/queue.ts
+++ apps/gateway/src/lib/queue.ts
@@ -1,5 +1,11 @@
 import { amqplib, RenderQueue } from "message-queue";
+import { RABBITMQ_HOSTNAME, RABBITMQ_PASSWORD, RABBITMQ_USERNAME } from './env';
+import consola from 'consola';
 
-const connection = await amqplib.connect("amqp://localhost");
+const connection = await amqplib.connect({
+  hostname: RABBITMQ_HOSTNAME,
+  username: RABBITMQ_USERNAME,
+  password: RABBITMQ_PASSWORD
+});
 
-export const renderQueue = await new RenderQueue(connection).init();
+export const renderQueue = await new RenderQueue(connection).init().catch(consola.error)
