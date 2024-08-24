776fcc060a3c552475d41759c264bc95aea92825
Erick Christian <erickchristian48@gmail.com>
2024-08-25 00:07:09 +0700

    feat(renderer): use env var for amqp

---
+++ apps/renderer/src/lib/env.ts
@@ -0,0 +1,7 @@
+import { env } from "common";
+
+
+export const RABBITMQ_HOSTNAME = env("RABBITMQ_HOSTNAME");
+export const RABBITMQ_USERNAME = env("RABBITMQ_USERNAME");
+export const RABBITMQ_PASSWORD = env("RABBITMQ_PASSWORD");
+
