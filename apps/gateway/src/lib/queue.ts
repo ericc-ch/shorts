9b68512a007380df7ca5edb0e540b4da339a3c8d
Erick Christian <erickchristian48@gmail.com>
2024-08-25 12:31:47 +0700

    feat(gateway): throw on queue connect fail

--- apps/gateway/src/lib/queue.ts
+++ apps/gateway/src/lib/queue.ts
@@ -1,11 +1,10 @@
 import { amqplib, RenderQueue } from "message-queue";
-import { RABBITMQ_HOSTNAME, RABBITMQ_PASSWORD, RABBITMQ_USERNAME } from './env';
-import consola from 'consola';
+import { RABBITMQ_HOSTNAME, RABBITMQ_PASSWORD, RABBITMQ_USERNAME } from "./env";
 
 const connection = await amqplib.connect({
   hostname: RABBITMQ_HOSTNAME,
   username: RABBITMQ_USERNAME,
-  password: RABBITMQ_PASSWORD
+  password: RABBITMQ_PASSWORD,
 });
 
-export const renderQueue = await new RenderQueue(connection).init().catch(consola.error)
+export const renderQueue = await new RenderQueue(connection).init();
