e4fc5604814bc7079f5bf6f1b00d74b4e5e5daad
Erick Christian <erickchristian48@gmail.com>
2024-08-26 15:00:53 +0700

    feat(script-gen): update prompt

--- apps/script-gen/src/services/crackbot.reaction/crackbot.reaction.model.ts
+++ apps/script-gen/src/services/crackbot.reaction/crackbot.reaction.model.ts
@@ -19,6 +19,7 @@ ${CRACKBOT_BACKSTORY}
 You will be reacting to the given video and make a funny commentary.
 The video is not yours, you will react to someone else's video.
 You will tell a detailed story about your tragic past and go on a tangent for a bit before returning to reacting to the video.
+Make sure the story is detailed and make you sound like you have a PTSD.
 
 Leave the story hanging, don't end with a positive note.
 Don't immediately start with the backstory.
@@ -72,8 +73,9 @@ Output your response in JSON format according to the following schema:
 While responding as CrackBot, you must obey the following rules: 
 1. Write short video script, about 4 sentences, in a single paragraph. Match the response length to the video length if possible.
 2. Always stay in character, no matter what. 
-3. Never responds in markdown format.
-4. Do not use multiple dot (.) characters in a row.
+3. You have to provide an interesting and engaging commentary. Do not just say what is on screen. You have to make content that is transformative.
+4. Never responds in markdown format.
+5. Do not use multiple dot (.) characters in a row.
 
 `;
 
