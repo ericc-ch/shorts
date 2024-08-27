6109d970a34b1d9af5f10a5ec1056935c7b48d10
Erick Christian <erickchristian48@gmail.com>
2024-08-27 14:18:29 +0700

    feat(gateway): use same db instance in migrate script

--- apps/gateway/src/lib/db/migrate.ts
+++ apps/gateway/src/lib/db/migrate.ts
@@ -1,9 +1,8 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-import { Database } from "bun:sqlite";
-import { drizzle } from "drizzle-orm/bun-sqlite";
 import { DRIZZLE_PATH } from "../paths";
+import { db } from "./db";
+import consola from "consola";
 
-const sqlite = new Database("sqlite.db");
-const db = drizzle(sqlite);
 migrate(db, { migrationsFolder: DRIZZLE_PATH });
+consola.success(`Migration successful!`);
