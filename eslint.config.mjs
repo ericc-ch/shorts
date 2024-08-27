1bab2281a073ec814cb18b222aadc86996987c03
Erick Christian <erickchristian48@gmail.com>
2024-08-27 19:50:26 +0700

    config(eslint): update to cover frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,47 @@
-// @ts-check
-
 import eslint from "@eslint/js";
 import perfectionist from "eslint-plugin-perfectionist";
 import prettier from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  {
-    ignores: ["eslint.config.*"],
-  },
-
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+  { ignores: ["**.config.*", "**/dist/"] },
 
   {
+    extends: [
+      eslint.configs.recommended,
+      perfectionist.configs["recommended-natural"],
+      ...tseslint.configs.recommended,
+      prettier,
+    ],
     languageOptions: {
       parserOptions: {
-        project: ["./apps/*/tsconfig.json", "./packages/*/tsconfig.json"],
+        project: ["./apps/*/tsconfig*.json", "./packages/*/tsconfig*.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-
-  {
-    ...perfectionist.configs["recommended-natural"],
     rules: {
       "perfectionist/sort-objects": ["error", { partitionByNewLine: true }],
     },
   },
-  prettier,
+
+  {
+    files: ["**/*.tsx"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
 );
