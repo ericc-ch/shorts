2f422df06ca2b7aa9dcea996022bd2f2c7c6d007
Erick Christian <erickchristian48@gmail.com>
2024-08-27 16:16:56 +0700

    feat(gateway): migrate on app start

--- apps/gateway/src/lib/db/db.ts
+++ apps/gateway/src/lib/db/db.ts
@@ -2,8 +2,17 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 
 import * as schema from "@/schemas";
-import { DB_PATH } from "../paths";
+import { DB_PATH, DRIZZLE_PATH } from "../paths";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import consola from "consola";
 
 const connection = new Database(DB_PATH);
 
 export const db = drizzle(connection, { schema });
+
+// Migrate here because
+// I don't think I can run it as a separate compose service
+// I shouldn't run migration in Dockerfile
+// I don't want the container command to have 2 process (migrate and start)
+migrate(db, { migrationsFolder: DRIZZLE_PATH });
+consola.success(`Migration successful!`);
--- apps/gateway/src/lib/db/migrate.ts
+++
@@ -1,8 +0,0 @@
-import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-
-import { DRIZZLE_PATH } from "../paths";
-import { db } from "./db";
-import consola from "consola";
-
-migrate(db, { migrationsFolder: DRIZZLE_PATH });
-consola.success(`Migration successful!`);
