c2a0355df0a1c9261ef29e21d2e4e5e4343a9833
Erick Christian <erickchristian48@gmail.com>
2024-08-27 12:40:46 +0700

    config(gateway): drizzle w/ sqlite for queue

---
+++ apps/gateway/drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+
+// Using npx because of this issue
+// https://github.com/oven-sh/bun/issues/7343
+
+export default defineConfig({
+  dbCredentials: {
+    url: `file:./database.db`,
+  },
+  dialect: "sqlite",
+  out: "./drizzle",
+  schema: "./src/schemas/*.schema.ts",
+});
