576235cdf080b2538cc9e7dea579b8bb59a506bb
Erick Christian <erickchristian48@gmail.com>
2024-08-28 00:54:48 +0700

    fix(eslint): correct glob pattern for ignoring configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["**.config.*", "**/dist/"] },
+  { ignores: ["**/*.config.*", "**/dist/"] },
 
   {
     extends: [
