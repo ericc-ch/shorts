fc59ec07778fa7b82c708a6414e02ca339057e53
Erick Christian <erickchristian48@gmail.com>
2024-08-28 12:58:05 +0700

    feat(gateway): remove basic auth

--- apps/gateway/src/index.ts
+++ apps/gateway/src/index.ts
@@ -1,26 +1,14 @@
 import type { Serve } from "bun";
 
 import { Hono } from "hono";
-import { basicAuth as basicAuthMiddleware } from "hono/basic-auth";
 import { logger } from "hono/logger";
 
 import { consumeProgress } from "./consume-progress";
-import {
-  GATEWAY_AUTH_PASSWORD,
-  GATEWAY_AUTH_USERNAME,
-  SERVER_PORT_GATEWAY,
-} from "./lib/env";
+import { SERVER_PORT_GATEWAY } from "./lib/env";
 import { routes } from "./routes";
 
 const app = new Hono();
 app.use("*", logger());
-app.use(
-  "*",
-  basicAuthMiddleware({
-    password: GATEWAY_AUTH_PASSWORD,
-    username: GATEWAY_AUTH_USERNAME,
-  }),
-);
 
 for (const route of routes) {
   app.route("/", route);
