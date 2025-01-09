79725fd08af65c746a7838d8c39006c306963b80
Erick Christian <erickchristian48@gmail.com>
2025-01-10 05:31:46 +0700

    refactor: [wip] migration to simpler architecture

--- apps/renderer/remotion.config.ts
+++
@@ -1,6 +0,0 @@
-import { Config } from "@remotion/cli/config";
-
-import { webpackOverride } from "./src/lib/webpack-override";
-
-Config.setEntryPoint("./src/remotion/index.ts");
-Config.overrideWebpackConfig(webpackOverride);
--- remotion.config.ts
+++ remotion.config.ts
@@ -1,12 +1,11 @@
 import { Config } from "@remotion/cli/config";
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
-export const webpackOverride: Parameters<
-  typeof Config.overrideWebpackConfig
->[0] = (config) => ({
+Config.setEntryPoint("./src/remotion/index.ts");
+Config.overrideWebpackConfig((config) => ({
   ...config,
   resolve: {
     ...config.resolve,
     plugins: [...(config.resolve?.plugins ?? []), new TsconfigPathsPlugin()],
   },
-});
+}));
